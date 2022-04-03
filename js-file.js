b6a81ae8af4124be4a1b1396e81df6659b96df4a
Kyaw Thu <evanch98@gmail.com>
2022-04-03 16:05:53 +0800

    Initial step of Player object

--- js-file.js
+++ js-file.js
@@ -4,12 +4,26 @@ const GameBoard = (() => {
     const gameBoard = [];
     for (let i=0; i < 9; i++) {
         const div = document.createElement('div');
+        div.classList.add('item');
+        div.setAttribute('id', i);
         div.textContent = "A";
         gameBoard.push(div);
     }
     return {gameBoard};
 })();
 
+const Player = (name, content) => {
+    const divs = document.querySelectorAll('.item');
+    divs.forEach((div) => {
+        div.addEventListener('click', () => {
+            div.textContent = content;
+        });
+    });
+    return {content};
+};
+
 for (div of GameBoard.gameBoard) {
     container.appendChild(div);
 }
+
+const player = Player('Evan', 'X');
